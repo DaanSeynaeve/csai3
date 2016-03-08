0694b07bfddb1d1d22e150ec1e336d40153c12ca
Daan Seynaeve <daanseynaeve2@hotmail.com>
2016-03-08 18:16:31 +0100

    generation and checking of random solutions

--- LAHC.R
+++ LAHC.R
@@ -1,8 +1,12 @@
-init <- function(instance) {
+# TODO
+# Generate an initial solution by generating new employee assignments
+# until every timeslot is sufficiently filled.
+init_solution <- function(instance) {
     print("hey")
 }
 
-new_dude <- function(instance) {
+# Generates a random assignment for the GDODOSP
+new_assignment <- function(instance) {
     print("making a new dude")
     assignment = rep(FALSE,instance$t)
     working <- sample(c(TRUE,FALSE),1)
@@ -17,16 +21,50 @@ new_dude <- function(instance) {
         }
         if (day==1) { min <- 1 }
         dur = sample(min:max,1)
-        dude[day:day-1+dur] = working
+        assignment[day:max(day-1+dur,instance$t)] = working
         day <- day + dur
         working <- !working
     }
+    print(assignment)
     return(assignment)
 }
-  
-lahc <- function() {
-    
+
+# Checks whether a given solutions solves the instance
+check_solution <- function(sol,instance) {
+    return(all(colSums(sol) >= instance$b))
 }
 
-sol <- function(sol)
+# Checks whether a given solutions consists of valid assignments
+check_assignments <- function(sol,instance) {
+    chk <- function(assignment) {check_assignment(assignment,instance)}
+    return(all(apply(check_assignment,sol)))
+}
+
+# Check whether a given assignment is valid
+check_assignment <- function(assignment,instance) {
+    pad_on <- rep(TRUE,instance$on_min)
+    pad_off <- rep(FALSE,instance$off_min)
+    freq <- function(x) { return(table(cumsum(x)[x==FALSE])) }
+    if (length(freq(!assignment)) > 0) {
+        c1 <- max(freq(!assignment)) <= instance$on_max
+        c2 <- min(freq(!c(pad_on,assignment,pad_on))) >= instance$on_min
+    } else {
+        c1 <- TRUE
+        c2 <- TRUE
+    }
+    if (length(freq(assignment)) > 0) {
+        c3 <- max(freq(assignment)) <= instance$off_max
+        c4 <- min(freq(c(pad_off,assignment,pad_off))) >= instance$off_min
+    } else {
+        c3 <- TRUE
+        c4 <- TRUE
+    }
+    return(all(c(c1,c2,c3,c4)))
+}
+ 
+# TODO
+# Run the LAHC algorithm
+lahc <- function() {
+    sol <- FALSE
     return(sol)
+}
