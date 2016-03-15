0e9d771df99c50f89269cbeb627ae83e8a004608
Tom <tom.decroos@hotmail.com>
2016-03-15 17:02:32 +0100

    sync

--- funwrapper.R
+++ funwrapper.R
@@ -1,13 +1,14 @@
-#args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
-args <- c(1,2,"--x",1,"--y",2)
-testinstance <- args[1]
-id <- args[2]
+instancefile <- args[1]
+instance <- readRDS(instancefile)
+#id <- args[2]
 i = 3
 while(i< length(args)) {
-  if(args[i]=="--x") x <- as.numeric(args[i+1])
-  if(args[i]=="--y") y <- as.numeric(args[i+1])
+  if(args[i]=="--lfa") lfa <- as.numeric(args[i+1])
+  #if(args[i]=="--maxit") maxit <- as.numeric(args[i+1])
   i<-i+2
 }
-source("testfun.R")
-cat(testf(testinstance,x,y))
+source("LAHC.R")
+maxit= 500
+cat(lahc(instance,lfa,maxit,FALSE))
