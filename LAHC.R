4901d3327c9d09e0365271551af411a49f23f0b2
Daan Seynaeve <daanseynaeve2@hotmail.com>
2016-03-26 12:53:14 +0100

    lshift for GSP

--- LAHC.R
+++ LAHC.R
@@ -3,8 +3,8 @@
 #' @param Lfa; length of the fitness array
 #' @param max_iterations; number of iterations to perform
 #' @param verbose; set to FALSE to surpress output
-lahc <- function(instance, Lfa, max_iterations, verbose) {
-    fn_mut = function(x) {find_neighbour(x,instance)}
+lahc <- function(instance, Lfa, max_iterations, p, verbose) {
+    fn_mut = function(x) {gsp_neighbour(x,instance,p)}
     fn_c = cost
     
     s <- init_solution(instance)
@@ -31,7 +31,7 @@ lahc <- function(instance, Lfa, max_iterations, verbose) {
         print('------ finished ------')
         print(fn_c(s))
     }
-    return(fn_c(s))
+    return(s)
 }
 
 #' Generate an initial solution by generating new employee assignments
@@ -61,27 +61,28 @@ cost2 <- function(sol, type) {
 #' Neighbourhoods
 #' --------------------------------
 
-find_neighbour <- function(sol, instance) {
-    and_ <- function(x){apply(x,2,all)}
-    or_ <- function(x){apply(x,2,any)}
-    xor_ <- function(x){apply(x,2,function(y) {any(y) && !all(y)})}
-    neighbourhood = sample(1:2,1)
-    if (TRUE) {
-        # print('replacement')
-        return(replace_assignment(sol,instance))
-        
-    } else {
-        # print('combination')
-        # return(vertical_swap(sol,instance))
-        # return(combine_assignments(sol,instance,and_))
-        return(bitflip(sol,instance))
-    }
-} 
+#' Combines several neighbourhoods for the GDODOSP
+gsp_neighbour <- function(sol, instance, p) {
+    # and_ <- function(x){apply(x,2,all)}
+    # or_ <- function(x){apply(x,2,any)}
+    # xor_ <- function(x){apply(x,2,function(y) {any(y) && !all(y)})}
+    neighbourhoods <- c(
+        function() {gsp_replace(sol,instance)},
+        function() {gsp_optimize(sol,instance)},
+        function() {gsp_lshift2(sol,instance)}
+    )
+    pcum <- cumsum(p)
+    # x <- sample(1:tail(pcum,n=1),1)
+    x <- runif(1,0,tail(pcum,n=1))
+    fn = min(which(pcum>x))
+    # print(fn)
+    return((neighbourhoods[[fn]])())
+}
 
 #' GDODOSP Neighbourhood function
 #' deletes a random employee assignment and
 #' generates new assignments until a feasible solution is reached
-replace_assignment <- function(sol, instance) {
+gsp_replace <- function(sol, instance) {
     r <- sample(1:dim(sol)[1],1)
     sol2 <- sol[-r,]
     return(repair(sol2, instance))
@@ -91,7 +92,7 @@ replace_assignment <- function(sol, instance) {
 #' combines two employees assignments into a new one and
 #' generates new assignments until a feasible solution is reached
 #' @param fn_combine; function that takes a matrix of 2 rows and combines them
-combine_assignments <- function(sol, instance, fn_combine) {
+gsp_combine <- function(sol, instance, fn_combine) {
     a <- sol[sample(1:dim(sol)[1],2),]
     b <- fn_combine(a)
     if (check_assignment(b,instance)) {
@@ -106,7 +107,7 @@ combine_assignments <- function(sol, instance, fn_combine) {
 
 #' GDODOSP Neighbourhood function
 #' swap the assignment of a timeslot between two employees
-vertical_swap <- function(sol, instance) {
+gsp_vswap <- function(sol, instance) {
     e <- sample(1:dim(sol)[1],2)
     d <- sample(1:dim(sol)[2],1)
     
@@ -127,9 +128,9 @@ vertical_swap <- function(sol, instance) {
     }
 }
 
-#' GDODSP Neighbourhood function
+#' GDODOSP Neighbourhood function
 #' randomly changes the assignment of an employee in a timeslot
-bitflip <- function(sol, instance) {
+gsp_bitflip <- function(sol, instance) {
     e <- sample(1:dim(sol)[1],1)
     d <- sample(1:dim(sol)[2],1)
     
@@ -143,10 +144,58 @@ bitflip <- function(sol, instance) {
     }
 }
 
-#' GDODSP Neighbourhood functon
+#' KAPOT
+#' GDOD0SP Neighbourhood functon
 #' shifts an existing employee assignment
-shift_assignment <- function(sol, instance) {
-    print('TODO')
+#' introduces a new assignment on the last day that fits the constraints
+gsp_lshift <- function(sol, instance) {
+    e <- sample(1:dim(sol)[1],1)
+    len <- dim(sol)[2]
+    last <- sol[e,len]
+    if (last) {
+        x <- tail(sol,instance$on_max)
+    } else {
+        x <- tail(sol,instance$off_max)
+    }
+    if (all(x==last)) {
+        new_last <- !last
+    } else {
+        new_last <- sample(c(TRUE,FALSE),1)
+    }
+    sol[e,] <- c(sol[e,2:len],new_last)
+    return(sol)
+}
+
+gsp_lshift2 <- function(sol, instace) {
+    e <- sample(1:dim(sol)[1],1)
+    len <- dim(sol)[2]
+    sol2 <- sol
+    sol2[e,] <- c(sol[e,2:len],sample(c(TRUE,FALSE),1))
+    if (check_solution(sol2,instace)) {
+        return(sol2)
+    } else {
+        return(sol)
+    }
+}
+
+#' CDODOSP Neighbourhood function
+#' shifts an employee assignment by 1
+csp_lshift <- function(sol, instance) {
+    len <- dim(sol)[2]
+    e <- sample(1:len,1)
+    sol[e,] <- c(sol[e,2:len],sol[e,1])
+    return(sol)
+}
+
+#' GDODOSP Neighbourhood function
+#' pick a random employee and remove him if possible
+gsp_optimize <- function(sol, instance) {
+    e <- sample(1:dim(sol)[1],1)
+    if (check_solution(sol[-e,], instance)) {
+        return(sol[-e,])
+    } else {
+        return(sol)
+    }
 }
 
 #' --------------------------------
