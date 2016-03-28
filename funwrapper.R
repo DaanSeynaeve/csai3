8e61bcb06da725d5c5f3e374e35a191ccd79e298
Tom <tom.decroos@hotmail.com>
2016-03-28 13:47:52 +0200

    sync

--- funwrapper.R
+++ funwrapper.R
@@ -6,10 +6,18 @@ instance <- readRDS(instancefile)
 i = 3
 while(i< length(args)) {
   if(args[i]=="--lfa") lfa <- as.numeric(args[i+1])
-  #if(args[i]=="--maxit") maxit <- as.numeric(args[i+1])
+  if(args[i]=="--p1") p1 <- as.numeric(args[i+1])
+  if(args[i]=="--p2") p2 <- as.numeric(args[i+1])
+  if(args[i]=="--p3") p3 <- as.numeric(args[i+1])
+  if(args[i]=="--p4") p4 <- as.numeric(args[i+1])
+  if(args[i]=="--p5") p5 <- as.numeric(args[i+1])
+  if(args[i]=="--p6") p6 <- as.numeric(args[i+1])
+  if(args[i]=="--p7") p7 <- as.numeric(args[i+1])
+  if(args[i]=="--p8") p8 <- as.numeric(args[i+1])
   i<-i+2
 }
 source("LAHC.R")
+source("GSP.R")
 maxit= 500
-p <- c(1,1,1,1,1,1,1,1)
+p <- c(p1,p2,p3,p4,p5,p6,p7,p8)
 cat(gsp_lahc(instance,lfa,maxit,p,FALSE)$res)
